9119c8bee78b75cbfbc99eaa9e4b574afdb8f068
carlossulbaran <carlossulbaran11@gmail.com>
2022-03-01 21:58:17 -0400

    creacion funcion GPS y cambio de paradigma

--- super_codigo.py
+++ super_codigo.py
@@ -1,1 +1,64 @@
-print("primera linea")
+import serial
+import numpy as np
+import math as mt
+from time import sleep
+
+def leer_gps():
+    try:
+        gps = serial.Serial(
+        port='/dev/ttyS0',
+        baudrate = 38400,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+            
+
+        ser_bytes = gps.readline()
+        decoded_bytes = ser_bytes.decode("utf-8")
+        data = decoded_bytes.split(",")
+                
+        if data[0] == '$GNRMC':
+            lat_nmea = data[3]
+            lat_grad = lat_nmea[:2]
+            lat_ddd = lat_nmea[2:10]
+            lat_mmm = float(lat_ddd) / 60
+                    
+        if data[6] == 'S':
+            latitud_grad = float(lat_grad) * -1
+            latitud = latitud_grad - lat_mmm
+
+        else:
+            latitud_grad = float(lat_grad)
+            latitud = latitud_grad + lat_mmm
+
+                        
+        long_nmea = data[5]
+        long_grad = long_nmea[1:3]
+        long_ddd = long_nmea[3:10]
+        long_mmm = float(long_ddd) / 60
+                    
+        if data[6] == 'W':
+            longitud_grad = float(long_grad) * -1
+            longitud = longitud_grad - long_mmm
+
+        else:
+            longitud_grad = float(long_grad)
+            longitud = longitud_grad + long_mmm
+
+
+            pos[0] = float(longitud)
+            pos[1] = float(latitud)
+
+        return pos
+
+                    
+    except serial.SerialException:
+        print("Fallo en comunicacion con el GPS!")
+
+while True:
+    pos = leer_gps()
+
+    print(pos)
+    time.sleep(2)
