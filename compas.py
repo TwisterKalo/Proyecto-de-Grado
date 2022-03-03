6e1a38560ac102dc9afafa261b67b0ca5a91dac8
carlossulbaran <carlossulbaran11@gmail.com>
2022-03-02 22:02:49 -0400

    cambio total a compas

--- compas.py
+++ compas.py
@@ -1,74 +1,61 @@
-import smbus		#import SMBus module of I2C
-from time import sleep  #import sleep
+import smbus #comunicación I2C
+from time import sleep
 import math
 
-#some MPU6050 Registers and their Address
-Register_A     = 0              #Address of Configuration register A
-Register_B     = 0x01           #Address of configuration register B
-Register_mode  = 0x02           #Address of mode register
+#direcciones requeridas
+#---------------config
+Registro_A = 0x0B
+Registro_B = 0x09
+RegStatus = 0x06
+RegCtrl = 0x09
+#---------------direcciones de la conexión
+bus=smbus.SMBus(1)
+deviceAdress = 0x0d
+#---------------datos
+eje_X_Mag = 0x00
+eje_Y_Mag = 0x02
+eje_Z_Mag = 0x04
+declination = -0.00669
+pi=3.14159265359
 
-X_axis_H    = 0x03              #Address of X-axis MSB data register
-Z_axis_H    = 0x05              #Address of Z-axis MSB data register
-Y_axis_H    = 0x07              #Address of Y-axis MSB data register
-declination = -0.00669          #define declination angle of location where measurement going to be done
-pi          = 3.14159265359     #define pi value
 
-
-def Magnetometer_Init():
-        #write to Configuration Register A
-        bus.write_byte_data(Device_Address, Register_A, 0x70)
-
-        #Write to Configuration Register B for gain
-        bus.write_byte_data(Device_Address, Register_B, 0xa0)
-
-        #Write to mode Register for selecting mode
-        bus.write_byte_data(Device_Address, Register_mode, 0)
-	
-	
+def MagnetometerInit():
+#configurar registro A
+bus.write_byte_data(deviceAdress, Registro_A, 0x01)
+#configurar registro B
+bus.write_byte_data(deviceAdress, Registro_B, 0x1D)
+#configurar registro para seleccionar el modo
+#bus.write_byte_data(deviceAdress, ModoRegistro, 0)
 
 def read_raw_data(addr):
-    
-        #Read raw 16-bit value
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
-
-        #concatenate higher and lower value
-        value = ((high << 8) | low)
-
-        #to get signed value from module
-        if(value > 32768):
-            value = value - 65536
-        return value
-
-
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x1e   # HMC5883L magnetometer device address
-
-Magnetometer_Init()     # initialize HMC5883L magnetometer 
-
-print (" Reading Heading Angle")
-
+#leer doble byte (16 bits)
+low = bus.read_byte_data(deviceAdress,addr)
+high = bus.read_byte_data(deviceAdress,addr+1)
+#concatenar los bytes
+valor = ((high << 8) | low)
+#obtener el signo
+if(valor > 32768):
+valor = valor - 65536
+return valor
+#------------------------------------------main
+MagnetometerInit()
+print('leyendo magnetometro...')
 while True:
-    
-	
-        #Read Accelerometer raw value
-        x = read_raw_data(X_axis_H)
-        z = read_raw_data(Z_axis_H)
-        y = read_raw_data(Y_axis_H)
-
-        heading = math.atan2(y, x) + declination
-        
-        #Due to declination check for >360 degree
-        if(heading > 2*pi):
-                heading = heading - 2*pi
-
-        #check for sign
-        if(heading < 0):
-                heading = heading + 2*pi
-
-        #convert into angle
-        heading_angle = int(heading * 180/pi)
-
-        print ("Heading Angle = %d°" %heading_angle)
-        sleep(1)
-        print("listo")
+bandera = bus.read_byte_data(deviceAdress,RegStatus)
+a="{0:b}".format(bandera)
+if a[len(a)-1] == 0:
+bandera = bus.read_byte_data(deviceAdress,RegStatus)
+x=read_raw_data(eje_X_Mag)
+y=read_raw_data(eje_Y_Mag)
+z=read_raw_data(eje_Z_Mag)
+heading = math.atan2(y,x)+ declination
+#compensar superiores a 360
+if(heading > 2*pi):
+heading = heding -2*pi
+#revisar el signo
+if(heading < 0):
+heading=heading+2*pi
+#convertir a grados
+heading_angle = int(heading * (180/pi))
+print("angulo = %d°" %heading_angle)
+sleep(0.5)
